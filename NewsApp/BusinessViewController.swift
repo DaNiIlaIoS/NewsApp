b2ae191ea3cf78a6c14239038285fc2d278b4836
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-06-05 21:10:17 +0500

    Add search bar and collectionView

--- NewsApp/BusinessViewController.swift
+++ NewsApp/BusinessViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 final class BusinessViewController: UIViewController {
+    // MARK: - GUI Variables
     
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Private Methods
 }
--- NewsApp/TechnologyViewController.swift
+++ NewsApp/TechnologyViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 final class TechnologyViewController: UIViewController {
+    // MARK: - GUI Variables
     
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Private Methods
 }
