7e6a6b1f1faea22bb0ac19dbb24c73b1c9d77dcb
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-06-06 11:59:35 +0500

    Add collection view cell to the general view controller

--- NewsApp/General/View/GeneralCollectionViewCell.swift
+++ NewsApp/General/View/GeneralCollectionViewCell.swift
@@ -10,11 +10,30 @@ import SnapKit
 
 final class GeneralCollectionViewCell: UICollectionViewCell {
     // MARK: - GUI Variables
+    private lazy var imageView: UIImageView = {
+       let image = UIImageView()
+        image.image = UIImage(named: "image") ?? UIImage.add
+        return image
+    }()
+    
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.5
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.textColor = .white
+        return label
+    }()
     
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
@@ -22,4 +41,27 @@ final class GeneralCollectionViewCell: UICollectionViewCell {
     }
     
     // MARK: - Private Methods
+    private func setupUI() {
+        addSubview(imageView)
+        addSubview(blackView)
+        addSubview(titleLabel)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.size.edges.equalToSuperview()
+        }
+        
+        blackView.snp.makeConstraints { make in
+            make.bottom.leading.trailing.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.bottom.equalTo(blackView)
+            make.leading.trailing.equalTo(blackView).offset(5)
+        }
+    }
 }
