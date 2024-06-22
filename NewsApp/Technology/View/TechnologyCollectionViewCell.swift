4cf6eeaa7a4ed00c450ce95f1c02926b7007fac2
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-06-22 18:26:07 +0500

    Add ui on view controller

---
+++ NewsApp/Technology/View/TechnologyCollectionViewCell.swift
@@ -0,0 +1,96 @@
+//
+//  TechnologyCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by Даниил Сивожелезов on 22.06.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class TechnologyCollectionViewCell: UICollectionViewCell {
+    // MARK: - GUI Variables
+    private lazy var imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "image")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.text = "12.04.2024 16:34"
+    
+        return label
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 2
+        label.text = "Some title"
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.numberOfLines = 0
+        label.text = "Description for the news Description for the news Description for the news Description for the news"
+        
+        return label
+    }()
+    
+    // MARK: - Properties
+    private let edgeInset = 10
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setupUI() {
+        addSubviews([imageView,
+                     titleLabel,
+                     dateLabel,
+                     descriptionLabel])
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(self.frame.width)
+            
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(5)
+            make.bottom.equalToSuperview().inset(edgeInset)
+        }
+    }
+}
